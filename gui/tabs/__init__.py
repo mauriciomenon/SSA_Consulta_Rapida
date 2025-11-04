eae1a64344ce831a634a3d611a034da5315076f9
Mauricio Menon <54405514+mauriciomenon@users.noreply.github.com>
2025-11-04 16:07:49 -0300

    Adicao de planilhas em docs_entrada e introducao de gui/helpers/ gui/mixins gui/tabs gui/widgets/ gui/workers e test_clean_tab.py

---
+++ gui/tabs/__init__.py
@@ -0,0 +1,7 @@
+# gui/tabs/__init__.py
+# Tab components for tabbed interface
+
+from gui.tabs.base_tab import BaseTab
+from gui.tabs.main_tab import MainTab
+
+__all__ = ['BaseTab', 'MainTab']
--- gui/widgets/__init__.py
+++ gui/widgets/__init__.py
@@ -5,5 +5,12 @@ from gui.widgets.column_manager_dialog import ColumnManagerDialog
 from gui.widgets.column_selector import ColumnSelector
 from gui.widgets.data_paginator import DataPaginator
 from gui.widgets.filter_help_dialog import FilterHelpDialog
+from gui.widgets.rescan_progress_dialog import RescanProgressDialog
 
-__all__ = ['ColumnManagerDialog', 'ColumnSelector', 'DataPaginator', 'FilterHelpDialog']
+__all__ = [
+    'ColumnManagerDialog',
+    'ColumnSelector',
+    'DataPaginator',
+    'FilterHelpDialog',
+    'RescanProgressDialog'
+]
